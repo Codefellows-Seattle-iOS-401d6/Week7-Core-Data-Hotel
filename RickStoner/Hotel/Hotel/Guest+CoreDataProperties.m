1e99c7b1ab290177ecd10aa799c1b2491cdea2c3
Rick Stoner <stonerrick19@gmail.com>
2016-07-18 15:14:29 -0700

    have data from json file being pulled in

---
+++ RickStoner/Hotel/Hotel/Guest+CoreDataProperties.m
@@ -0,0 +1,19 @@
+//
+//  Guest+CoreDataProperties.m
+//  Hotel
+//
+//  Created by Rick  on 7/18/16.
+//  Copyright © 2016 Rick . All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+#import "Guest+CoreDataProperties.h"
+
+@implementation Guest (CoreDataProperties)
+
+@dynamic name;
+@dynamic reservation;
+
+@end
---
+++ RickStoner/Hotel/Hotel/Room.m
@@ -0,0 +1,17 @@
+//
+//  Room.m
+//  Hotel
+//
+//  Created by Rick  on 7/18/16.
+//  Copyright © 2016 Rick . All rights reserved.
+//
+
+#import "Room.h"
+#import "Hotel.h"
+#import "Reservation.h"
+
+@implementation Room
+
+// Insert code here to add functionality to your managed object subclass
+
+@end
