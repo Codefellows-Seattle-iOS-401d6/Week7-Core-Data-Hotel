1f2c3a1b968ca8eb9c023403208b76c500d4d9ae
Rick Stoner <stonerrick19@gmail.com>
2016-07-18 14:27:12 -0700

    initial commit

---
+++ RickStoner/Hotel/Hotel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Hotel
+//
+//  Created by Rick  on 7/18/16.
+//  Copyright © 2016 Rick . All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
