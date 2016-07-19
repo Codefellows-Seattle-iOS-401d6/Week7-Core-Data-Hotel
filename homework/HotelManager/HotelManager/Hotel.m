34257bb21e9b7535dc5a37d42034fc5b08a7fc83
sungkim3 <sng.kim103@gmail.com>
2016-07-18 17:28:24 -0700

    completes week7 class 1 lab assignment

---
+++ homework/HotelManager/HotelManager/Hotel.m
@@ -0,0 +1,15 @@
+//
+//  Hotel.m
+//  HotelManager
+//
+//  Created by Sung Kim on 7/18/16.
+//  Copyright © 2016 Sung Kim. All rights reserved.
+//
+
+#import "Hotel.h"
+
+@implementation Hotel
+
+// Insert code here to add functionality to your managed object subclass
+
+@end
