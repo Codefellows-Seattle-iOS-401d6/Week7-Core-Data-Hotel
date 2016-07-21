9d92c5abc4f3f2333cf81a25a99a7de8b415f50d
Olesia Kalashnik <seventhform@gmail.com>
2016-07-20 20:34:54 -0700

    Project for Class3 Week7

---
+++ OlesiaKalashnik/HotelManager/Hotel.m
@@ -0,0 +1,16 @@
+//
+//  Hotel.m
+//  HotelManager
+//
+//  Created by Olesia Kalashnik on 7/19/16.
+//  Copyright © 2016 Olesia Kalashnik. All rights reserved.
+//
+
+#import "Hotel.h"
+#import "Room.h"
+
+@implementation Hotel
+
+// Insert code here to add functionality to your managed object subclass
+
+@end
